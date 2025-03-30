b36ec2f829a400e831b3d12fa061b65fd14d75b4
Neea <neearusch@gmail.com>
2025-03-29 23:01:12 -0400

    edit guide

--- code/guide.c
+++ code/guide.c
@@ -1,8 +1,6 @@
 void main(int X1, int X2, int X3){
-    // initial values  ↑
-    /*
-     * various commands involving
-     * variables X1, X2, X3
-     */
+    // initial values ↑
+    /* various commands involving
+       variables X1, X2, X3 */
     // X1', X2', X3' (final values)
 }
