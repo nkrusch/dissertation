c8cf77fd38e72a87ddcdb17725b57477c6346ef1
Neea <neearusch@gmail.com>
2025-07-27 22:07:56 -0400

    use template for draft

--- code/lucid.dfy
+++ code/lucid.dfy
@@ -1,4 +1,5 @@
 method LucidLoop(X1: nat, X2: nat, X3: nat, X4: nat, X5: nat){
+
   // for bookkeeping -- record initial values
   var X1', X2', X3', X4', X5' := X1, X2, X3, X4, X5;
 
--- code/notinfinite4.c
+++ code/notinfinite4.c
@@ -1,7 +1,6 @@
 int loop(int X1, int X2, int X3,
          int X4, int X5) {
-  while(__VERIFIER_nondet_int())
-  {
+  while(__VERIFIER_nondet_int()){
       X1 = X2 + X2;
       X2 = X3 + X3;
       X4 = X5 + X5;
